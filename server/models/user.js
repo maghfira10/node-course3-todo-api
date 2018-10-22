d1be7ea26bc1f9295c5ade944af8206b55d2ec90
maghfira10 <maghfiradizzania@alumni.pcr.ac.id>
2018-10-22 16:53:59 +0700

    Hash user password

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
-
+const bcrypt = require ('bcryptjs');
 var UserSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -73,6 +73,22 @@ return User.findOne({
     'tokens.access' : 'auth'
 });
 };
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+   if (user.isModified('password')) {
+      bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(user.password, salt, (err, hash) => {
+              user.password = hash;
+              next ();
+          })
+      })
+   }
+   else {
+       next();
+   }
+})
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User}
